4a37fa61458b910e798b745ed94a16b1bbecd2fc
diego.smelo <diego.smelo@OSA0545042W10-1>
2018-02-06 15:03:23 -0200

    calc

---
+++ CalcDiegoDeSouza/CalcDiegoDeSouza/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using CalcDiegoDeSouza.ModelView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalcDiegoDeSouza
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new ViewModel();
+
+
+        }
+    }
+}
